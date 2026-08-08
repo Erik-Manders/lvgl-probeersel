fff2e3e474127fd8478fdd27f1a20e7719a423d1
erik <nvt>
2026-08-08 15:16:33 +0100

    main.cpp toegevoegd

---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+
+#include <Arduino.h>
+
+#include <lvgl.h>
+#include <TFT_eSPI.h>
+#include <XPT2046_Touchscreen.h>
+#include "ui/ui.h" // Geïmporteerde SquareLine bestanden
+
+// Touchscreen pinnen voor CYD
+#define XPT2046_IRQ 36
+#define XPT2046_MOSI 32
+#define XPT2046_MISO 39
+#define XPT2046_CLK 25
+#define XPT2046_CS 33
+
+SPIClass mySpi = SPIClass(VSPI);
+XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
+TFT_eSPI tft = TFT_eSPI();
+
+static const uint16_t screenWidth  = 320;
+static const uint16_t screenHeight = 240;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * 10];
+
+// Display Flush Callback (stuurt pixels naar TFT_eSPI)
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
+    tft.endWrite();
+    lv_disp_flush_ready(disp);
+}
+
+// Touchpad Read Callback
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+    if (ts.touched()) {
+        TS_Point p = ts.getPoint();
+        // Kalibratie en schalen voor het CYD scherm
+        data->point.x = map(p.x, 200, 3700, 0, screenWidth);
+        data->point.y = map(p.y, 240, 3800, 0, screenHeight);
+        data->state = LV_INDEV_STATE_PR;
+    } else {
+        data->state = LV_INDEV_STATE_REL;
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // Initialiseer TFT & Touch
+    tft.begin();
+    tft.setRotation(1); // Lanschap modus
+    mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+    ts.begin(mySpi);
+    ts.setRotation(1);
+
+    // Initialiseer LVGL
+    lv_init();
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 10);
+
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    // Start de SquareLine UI!
+    ui_init();
+}
+
+void loop() {
+    lv_timer_handler(); // Houdt LVGL en de UI actief
+    delay(5);
+}
+
+// Event-functie aangeroepen vanuit SquareLine UI (indien gedefinieerd)
+// void btn_click_action(lv_event_t * e) {
+//     Serial.println("Knop geklikt op het CYD scherm!");
+// }
